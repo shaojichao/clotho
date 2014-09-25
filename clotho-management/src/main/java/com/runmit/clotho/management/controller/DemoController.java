5cba95597eaf0ad1a469db7754f941882b7b2bd7
zhipeng.tian <zhipeng.tian@runmit.com>
2014-09-25 14:16:18 +0800

    format code

--- clotho-management/src/main/java/com/runmit/clotho/management/controller/DemoController.java
+++ clotho-management/src/main/java/com/runmit/clotho/management/controller/DemoController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.runmit.clotho.core.domain.CurrentStatus;
