a93e9a70f3c9b83046791512b7a415205155f43e
mengheng.li <mengheng.li@runmit.com>
2014-10-14 18:03:48 +0800

    add serverfile page

---
+++ clotho-management/src/main/java/com/runmit/clotho/management/controller/cdn/RedirectServerFileController.java
@@ -0,0 +1,24 @@
+package com.runmit.clotho.management.controller.cdn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author mengheng.li@runmit.com 2014/10/14 10:31
+ */
+@Controller
+@Component
+public class RedirectServerFileController {
+	private static final Logger LOGGER = LoggerFactory
+            .getLogger(RedirectServerFileController.class);
+	
+	@RequestMapping(value = "/cdn/serverfile.do")
+	public String toServerFile() {
+		LOGGER.info("List Server File");
+		return "cdn/serverfile";
+	}
+
+}
---
+++ clotho-management/src/main/webapp/static/js/cdn/serverfile.js
@@ -0,0 +1,44 @@
+var centerPanel = Ext.create('Ext.grid.Panel', {
+	region: 'center',
+	title: '服务器文件列表',
+    columns: [
+		        {header: 'ID',  dataIndex: 'id', width: 60,sortable:true },
+		        {header: '服务器IP（外网）', dataIndex: 'ipWan', width: 150},
+                {header: '服务器IP（内网）', dataIndex: 'ipLan', width: 150},
+		        {header: '文件', dataIndex: 'fileName', width: 600},
+		        {header: '激活状态',  dataIndex: 'active', width: 100,sortable:true }
+		     ],
+	store: Ext.create('Ext.data.JsonStore', {
+		autoLoad: true,
+		storeId: 'centerStore',
+		pageSize: 20,
+	    fields :['id', 'ipWan','ipLan','fileName','active'],
+	    proxy: {
+	        type: 'ajax',
+	        url: '/abc/cdn_management/serverFile/list',
+	        reader: {
+	            type: 'json',
+	            totalProperty: "result",
+	            root: 'rows'
+	        }
+	    }
+	}),
+	bbar: Ext.create('Ext.toolbar.Paging', {//xtype: pagingtoolbar
+        store: Ext.data.StoreManager.get('centerStore'),
+        displayInfo: true,
+        displayMsg: '第{0}-{1}条，共{2}条',
+        emptyMsg: "没有数据",
+        beforePageText: '第',
+        afterPageText: '页，共 {0} 页'
+    }),
+    listeners: {
+    }
+});
+
+//domReady
+Ext.onReady(function(){
+	Ext.create('Ext.container.Viewport', {
+		layout: 'border',
+		items:[centerPanel]
+	});
+});
