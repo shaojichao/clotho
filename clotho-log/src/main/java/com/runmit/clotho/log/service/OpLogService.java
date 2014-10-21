960bf52753d1fe17405cb751152f4627bab65e84
zhipeng.tian <zhipeng.tian@runmit.com>
2014-10-21 15:35:31 +0800

    method para introduction

--- clotho-log/src/main/java/com/runmit/clotho/log/service/OpLogService.java
+++ clotho-log/src/main/java/com/runmit/clotho/log/service/OpLogService.java
@@ -22,12 +22,28 @@ public class OpLogService{
 	@Autowired
 	private OpLogMapper mapper;
 	
+	/**
+	 * 
+	 * @param content log内容
+	 * @param opType 操作类型
+	 * @param opMod 模块
+	 * @param systemId 系统
+	 * @param createby 操作人
+	 */
 	@Transactional(readOnly=false)
 	public void saveOpLog(String content,OpType opType,String opMod,String systemId,String createby){
 		OpLog opLog = new OpLog(content,createby,opType,systemId,opMod);
 		this.mapper.addOpLog(opLog);
 	}
 	
+	/**
+	 * 
+	 * @param obj 操作后的对象
+	 * @param opType 操作类型
+	 * @param opMod 模块
+	 * @param systemId 系统
+	 * @param createby 操作人
+	 */
 	@Transactional(readOnly=false)
 	public void saveObj(Object obj,OpType opType,String opMod,String systemId,String createby){
 		Gson gson = new Gson();
@@ -36,6 +52,15 @@ public class OpLogService{
 		this.mapper.addOpLog(opLog);
 	}
 	
+	/**
+	 * 
+	 * @param objbefore 操作前的对象
+	 * @param objafter 操作后的对象
+	 * @param opType 操作类型
+	 * @param opMod 模块
+	 * @param systemId 系统
+	 * @param createby 操作人
+	 */
 	@Transactional(readOnly=false)
 	public void updateObj(Object objbefore,Object objafter,OpType opType,String opMod,String systemId,String createby){
 		Gson gson = new Gson();
