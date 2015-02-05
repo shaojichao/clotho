d90cd72491d88f8d9e54ad976042d6b3bd5476c9
zhipeng.tian <zhipeng.tian@runmit.com>
2015-02-05 15:40:53 +0800

    add json

--- clotho-management/src/main/java/com/runmit/clotho/management/controller/clotho/UpgradeController.java
+++ clotho-management/src/main/java/com/runmit/clotho/management/controller/clotho/UpgradeController.java
@@ -9,6 +9,7 @@ import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -273,7 +274,7 @@ public class UpgradeController {
 		return entity;
 	}
 	
-	@RequestMapping(value = "/cdnback.do", method = RequestMethod.POST)
+	@RequestMapping(value = "/cdnback.do", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Object cdnback(CDNBackRes back,
 			HttpServletRequest request) {
 		LOGGER.debug((new Gson()).toJson(back));
