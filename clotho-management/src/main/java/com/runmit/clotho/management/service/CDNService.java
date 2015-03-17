8d824941cf21158a34d0479c4dc93f438776fd5f
zhipeng.tian <zhipeng.tian@runmit.com>
2015-03-17 15:38:26 +0800

    add ts to sing

--- clotho-management/src/main/java/com/runmit/clotho/management/service/CDNService.java
+++ clotho-management/src/main/java/com/runmit/clotho/management/service/CDNService.java
@@ -61,7 +61,7 @@ public class CDNService {
 		String hwid = version.getId() + "";
 		int appid = version.getClientid();
 		String appkey = version.getVersion();
-		String key = DigestUtils.md5DigestAsHex((hwid+version.getPkgurl()+gslbkey+appid+appkey).getBytes());
+		String key = DigestUtils.md5DigestAsHex((hwid+ts+version.getPkgurl()+gslbkey+appid+appkey).getBytes());
 		StringBuffer sb = new StringBuffer();
 		sb.append(gslburl).append(version.getPkgurl()).append("?appid=")
 				.append(appid).append("&appkey=").append(appkey)
@@ -69,5 +69,4 @@ public class CDNService {
 				.append(ts).append("&key=").append(key);
 		return sb.toString();
 	}
-	
 }
