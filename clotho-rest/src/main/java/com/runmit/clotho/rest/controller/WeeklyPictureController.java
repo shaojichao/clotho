8f4e3685e68ae1427de73b5d5ae0f00d4d9691cc
HongBin.Cao <HongBin.Cao@runmit.com>
2015-04-09 14:36:41 +0800

    每周一图的上传后台及接口。Ⅰ期功能 冗余返回值去除

--- clotho-rest/src/main/java/com/runmit/clotho/rest/controller/WeeklyPictureController.java
+++ clotho-rest/src/main/java/com/runmit/clotho/rest/controller/WeeklyPictureController.java
@@ -1,11 +1,14 @@
 package com.runmit.clotho.rest.controller;
 
+import com.runmit.clotho.core.domain.BaseDomain;
 import com.runmit.clotho.core.domain.picture.WeeklyPicture;
 import com.runmit.clotho.core.service.WeeklyPictureService;
 import com.runmit.clotho.rest.common.RestConst;
 import com.runmit.clotho.rest.domain.CommonResp;
+import com.runmit.clotho.rest.domain.RespWeeklyPicture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +36,11 @@ public class WeeklyPictureController {
     	CommonResp resp = new CommonResp();
     	try{
     		WeeklyPicture pictureWeekly = this.weeklyPictureService.getPictureWeekly();
-    		resp.setData(pictureWeekly);
+            RespWeeklyPicture rep = new RespWeeklyPicture();
+            /* 属性重组 */
+            BeanUtils.copyProperties(pictureWeekly, rep);
+            rep.setId(pictureWeekly.getId().toString());
+    		resp.setData(rep);
     		resp.setRtn(RestConst.RTN_OK);
             resp.setRtmsg("操作成功");
             LOGGER.info("getPictureList succeed");
---
+++ clotho-rest/src/main/java/com/runmit/clotho/rest/domain/RespWeeklyPicture.java
@@ -0,0 +1,16 @@
+package com.runmit.clotho.rest.domain;
+
+import lombok.Data;
+
+/**
+ * @author hongbin.cao
+ *
+ * @date 2015年4月7日
+ */
+@Data
+public class RespWeeklyPicture {
+	private String id;
+    private String url;
+    private String comment;
+    private long filesize;
+}
