bb7d8db58d918ab47652bd6d03dbd0fd9d202292
zhipeng.tian <zhipeng.tian@runmit.com>
2015-06-16 16:03:58 +0800

    mybatis cache setting

--- clotho-core/src/main/java/com/runmit/clotho/core/mapper/ClientMapper.java
+++ clotho-core/src/main/java/com/runmit/clotho/core/mapper/ClientMapper.java
@@ -2,6 +2,7 @@ package com.runmit.clotho.core.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
@@ -13,6 +14,7 @@ import com.runmit.clotho.core.domain.client.Client;
  *
  * @author zhipeng.tian
  */
+@CacheNamespace(implementation = org.mybatis.caches.memcached.MemcachedCache.class)
 public interface ClientMapper {
 
     @Insert("INSERT INTO Client (`name`,`superProjectId`,`description`,`createby`) "
@@ -25,8 +27,10 @@ public interface ClientMapper {
     void updateClient(Client client);
     
     @Select("SELECT * FROM Client")
+    @Options(useCache = true, flushCache = false)
     List<Client> getList();
     
     @Select("SELECT * FROM Client where clientId=#{clientId}")
+    @Options(useCache = true, flushCache = false)
     Client getClient(@Param("clientId")int clientId);
 }
